1e6aebb7f9d8aef3b27f4428b1a70c8078abc77e
Schnretzl <wrhendley@gmail.com>
2024-08-13 10:58:19 -0500

    Add problem 3

---
+++ problem3.py
@@ -0,0 +1,20 @@
+# 3. Arithmetic Operations in Daily Life
+
+# Task 1: Grocery Store Math
+avocado = 2.12
+steak = 13.64
+cheese = 2.49
+
+print("Avocado: ", avocado)
+print("Steak: ", steak)
+print("Cheese: ", cheese)
+print("Total: ", avocado + steak + cheese)
+
+
+# Task 2: Bank Interest
+initial_amount = 15000
+interest_rate = 0.05
+
+print("Initial Amount: ", initial_amount)
+print("Annual interest Rate: ", interest_rate)
+print("Amount after one year: ", initial_amount * (1 + interest_rate))
