1d68468d7c330e9a17def3f7e9594d3977bde825
Schnretzl <wrhendley@gmail.com>
2024-08-13 10:23:04 -0500

    Add problem 1

---
+++ problem1.py
@@ -0,0 +1,17 @@
+# 1. Showcase Your Dance Moves!
+
+# Task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+    
+
+# Task 2
+mood = input("How are you feeling today?")
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
