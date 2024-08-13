6c0a06ce029d30e3f1f5ca5c6261b2be1a4caa89
Schnretzl <wrhendley@gmail.com>
2024-08-13 10:41:58 -0500

    Add problem 2

---
+++ problem2.py
@@ -0,0 +1,7 @@
+# 2. Python Naming Convention Practice
+
+# Task 1: Code Correction
+pi_value = 3.14             # variables should not be capitalized
+user_age = 25               # variables should use snake_case rather than camelCase
+user_location = "New York"  # variable correctly uses snake_case
+MAXLIMIT = 1000             # constant correctly uses CAPITALS
